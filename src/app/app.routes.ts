aba788b4fe619e3bf371ae470a4605599bdc0fa1
jjriofrio <jjriofrio@uees.edu.ec>
2024-06-18 08:36:34 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { NoticiasComponent } from './componentes/noticias/noticias.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+        { path: '', component: InicioComponent },
+        { path: 'nosotros', component: NosotrosComponent },
+        { path: 'noticias', component: NoticiasComponent }
+];
